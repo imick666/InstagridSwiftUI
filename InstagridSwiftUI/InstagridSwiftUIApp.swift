ebe219907dd857090fdb68914afb8f15d8107f5e
Ruzel Mickael <mickael.ruzel@gmail.com>
2021-01-26 11:12:35 +0100

    Initial Commit

---
+++ InstagridSwiftUI/InstagridSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagridSwiftUIApp.swift
+//  InstagridSwiftUI
+//
+//  Created by mickael ruzel on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagridSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
